bfc67d17886b88a77244826696e7e5a328811214
jj_ <avasoldier2@gmail.com>
2022-04-03 10:49:00 +0900

    first commit

---
+++ lib/mc_server.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mc_server/version"
+
+module McServer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mc_server/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module McServer
+  VERSION = "0.1.0"
+end
